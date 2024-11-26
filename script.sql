8f29618714434bf6c0fd1bdc7bda226a5ce0ff8e
fernando7fatec <fernando.ramos9@fatec.sp.gov.br>
2024-11-26 09:23:14 -0300

    feat(p2): implements question # 2

--- script.sql
+++ script.sql
@@ -13,3 +13,19 @@ CREATE TABLE students_performance(
     prep_exam INT,
     classroom INT,
     grade INT);
+
+-- Question # 2 
+DROP PROCEDURE IF EXISTS sp_numero_aluno_aprovado;
+
+CREATE OR REPLACE PROCEDURE sp_numero_aluno_aprovado()
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	alunos INT;
+BEGIN
+    SELECT count(id) INTO alunos from students_performance WHERE father_edu = 6 or mother_edu = 6 AND grade >= 3.49;
+    RAISE NOTICE 'Os alunos aprovados são os %', alunos;
+END;
+$$
+
+CALL sp_numero_aluno_aprovado();
