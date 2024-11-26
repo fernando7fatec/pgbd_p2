8e6ae7cdd0441b3ff4cdd8458ab74ab327301362
fernando7fatec <fernando.ramos9@fatec.sp.gov.br>
2024-11-26 08:44:43 -0300

    feat(p2): tabela para importacao dos dados

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Arthur Domingos F. Ramos RA # 2041382311019
-# Fernando R. RA # 2041382321018
-# Théo Santos Dell Acqua - RA#2041382321031
+# Fernando_ Ramos_ RA # 2041382321018
+# Théo Santos Dell Acqua - RA # 2041382321031
---
+++ script.sql
@@ -0,0 +1,15 @@
+-- CREATE students_performance table
+CREATE TABLE students_performance(
+    id SERIAL PRIMARY KEY,
+    age INT,
+    scholarship INT,
+    partner INT,
+    salary INT,
+    mother_edu INT,
+    father_edu INT,
+    read_freq INT,
+    read_freq_sci INT,
+    prep_study INT,
+    prep_exam INT,
+    classroom INT,
+    grade INT);
